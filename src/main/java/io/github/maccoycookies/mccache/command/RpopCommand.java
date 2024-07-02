fcca08da885ced35d702daf2487e95107f6c113d
Maccoy <maccoy@foxmail.com>
2024-07-02 22:25:02 +0800

    rpop bugfix

--- src/main/java/io/github/maccoycookies/mccache/command/RpopCommand.java
+++ src/main/java/io/github/maccoycookies/mccache/command/RpopCommand.java
@@ -7,7 +7,7 @@ import io.github.maccoycookies.mccache.core.Reply;
 public class RpopCommand implements Command {
 
     public String name() {
-        return "LPOP";
+        return "RPOP";
     }
 
     @Override
